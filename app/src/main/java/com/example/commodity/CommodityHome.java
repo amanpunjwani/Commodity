d5d493a03b8ebc87b64811496ab8da6f98746564
SameerHaq349 <sameerhaq08@gmail.com>
2021-03-07 19:33:53 -0600

    Commodity Login/Signup page

---
+++ app/src/main/java/com/example/commodity/CommodityHome.java
@@ -0,0 +1,44 @@
+package com.example.commodity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CommodityHome extends AppCompatActivity  {
+    Button btnLogout;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commodity_home);
+
+        btnLogout = findViewById(R.id.logOut);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(CommodityHome.this, MainActivity.class);
+                startActivity(intToMain);
+            }
+        });
+    }
+
+
+}
